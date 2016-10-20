39ce03820951673e740c4638d68d889b8fd00b74
root <root@xiocwan-slave-preserve.novalocal>
2016-10-19 21:36:47 -0400

    add history logs

---
+++ load_env.sh
@@ -0,0 +1,2 @@
+c=`oc version` &&  OC=`echo $c|awk '{print $2}'`
+adm=`oc version` &&  OADM=`echo $adm|awk '{print $2}'`
---
+++ load_env_pre.sh
@@ -0,0 +1,2 @@
+c=`oc version` &&  oldoc=`echo $c|awk '{print $2}'`
+adm=`oc version` &&  oldoadm=`echo $adm|awk '{print $2}'`
